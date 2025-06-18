6ac6584ea6bc6f7180b3e3e120b028a67414aa94
Rui Xu <rui@nextbillion.ai>
2025-06-18 20:53:29 +0800

    feat: add negative value return

--- binary.go
+++ binary.go
@@ -3,12 +3,12 @@ package yog
 import (
 	"bytes"
 	"encoding/binary"
-	"encoding/json"
-	"errors"
-	"fmt"
 )
 
-func decodeChunk(input []byte) ([]uint32, []uint32, error) {
+type Uint32BinarySerializer struct {
+}
+
+func (b Uint32BinarySerializer) decodeChunk(input []byte) ([]uint32, []uint32, error) {
 	var distances = make([]uint32, len(input)/8)
 	var durations = make([]uint32, len(input)/8)
 
@@ -29,7 +29,7 @@ func decodeChunk(input []byte) ([]uint32, []uint32, error) {
 	return durations, distances, nil
 }
 
-func encodeChunk(durations, distances []uint32) ([]byte, error) {
+func (b Uint32BinarySerializer) encodeChunk(durations, distances []int32) ([]byte, error) {
 	var binBuf bytes.Buffer
 
 	for i := range durations {
@@ -46,7 +46,7 @@ func encodeChunk(durations, distances []uint32) ([]byte, error) {
 	return binBuf.Bytes(), nil
 }
 
-func decode(input []byte) (uint32, uint32, error) {
+func (b Uint32BinarySerializer) decode(input []byte) (uint32, uint32, error) {
 	buffer0 := bytes.NewBuffer(input[0:4])
 	buffer1 := bytes.NewBuffer(input[4:8])
 	var duration, distance uint32
@@ -62,7 +62,7 @@ func decode(input []byte) (uint32, uint32, error) {
 	return duration, distance, nil
 }
 
-func encode(duration, distance uint32) ([]byte, error) {
+func (b Uint32BinarySerializer) encode(duration, distance int32) ([]byte, error) {
 	var binBuf bytes.Buffer
 	err := binary.Write(&binBuf, binary.LittleEndian, duration)
 	if err != nil {
@@ -75,113 +75,72 @@ func encode(duration, distance uint32) ([]byte, error) {
 	return binBuf.Bytes(), nil
 }
 
-// Encode MatrixInfo Result -> binary
-// i = o * d.length + d
-func EncodeNumber(data string) ([]byte, error) {
-
-	var resp MatrixData
-	err := json.Unmarshal([]byte(data), &resp)
-	if err != nil {
-		return nil, err
-	}
-
-	result := ""
-	for _, row := range resp.Rows {
-		rowStr := "|"
-		for _, e := range row.Elements {
-			rowStr = preByteConcat(rowStr, fmt.Sprintf("%v,%v|", e.Duration.Value, e.Distance.Value))
-		}
-		result = preByteConcat(result, rowStr)
-		result = preByteConcat(result, "\n")
-	}
-	return []byte(result), nil
+type Int32BinarySerializer struct {
 }
 
-func preByteConcat(str1, str2 string) string {
-	buf := []byte(str1)
-	buf = append(buf, str2...)
-	return string(buf)
-}
+func (b Int32BinarySerializer) decodeChunk(input []byte) ([]int32, []int32, error) {
+	var distances = make([]int32, len(input)/8)
+	var durations = make([]int32, len(input)/8)
 
-// Encode MatrixInfo Result -> binary
-// i = o * d.length + d
-func Encode(data string) ([]byte, error) {
+	for i := 0; i < len(input); i += 8 {
+		buffer0 := bytes.NewBuffer(input[i : i+4])
+		buffer1 := bytes.NewBuffer(input[i+4 : i+8])
 
-	var resp MatrixData
-	err := json.Unmarshal([]byte(data), &resp)
-	if err != nil {
-		return nil, err
-	}
-
-	header, err := encode(uint32(len(resp.Rows)), uint32(len(resp.Rows[0].Elements)))
-	if err != nil {
-		return nil, err
-	}
-
-	// 将 MatrixData.Rows 转化成 binary
-	res := make([]byte, 0)
-
-	// add header
-	res = append(res, header...)
-
-	// source index
-	for _, row := range resp.Rows {
-		// destination index
-		for _, element := range row.Elements {
-			chunk, err := encode(element.Duration.Value, element.Distance.Value)
-			if err != nil {
-				return nil, err
-			}
-			res = append(res, chunk...)
-		}
-	}
-	return res, nil
-}
-
-// Decode binary -> MatrixInfo Result
-func Decode(bin []byte) (string, error) {
-
-	// check data
-	if len(bin)%8 > 0 {
-		return "", errors.New("illegal binary data format")
-	}
-
-	//read header
-	sourceLength, destinationLength, err := decode(bin[0:8])
-	if err != nil {
-		return "", errors.New("binary header decode failed")
-	}
-	bin = bin[8:]
-
-	// resize result
-	var m MatrixData
-	m.Rows = make([]MatrixRow, sourceLength)
-	for i := range m.Rows {
-		m.Rows[i].Elements = make([]MatrixElement, destinationLength)
-	}
-
-	// decode
-	for i := 0; i < len(bin); i = i + 8 {
-		duration, distance, err := decode(bin[i : i+8])
+		err := binary.Read(buffer0, binary.LittleEndian, &durations[i/8])
 		if err != nil {
-			return "", errors.New("binary decode failed")
+			return nil, nil, err
 		}
 
-		n := uint32(i / 8)
-		var d = n % destinationLength
-		var s = (n - d) / destinationLength
-		//fmt.Println(fmt.Sprintf("s=%v, d=%v", s, d))
+		err = binary.Read(buffer1, binary.LittleEndian, &distances[i/8])
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+	return durations, distances, nil
+}
 
-		m.Rows[s].Elements[d] = MatrixElement{
-			Distance: Value{distance},
-			Duration: Value{duration},
+func (b Int32BinarySerializer) encodeChunk(durations, distances []int32) ([]byte, error) {
+	var binBuf bytes.Buffer
+
+	for i := range durations {
+		err := binary.Write(&binBuf, binary.LittleEndian, durations[i])
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(&binBuf, binary.LittleEndian, distances[i])
+		if err != nil {
+			return nil, err
 		}
 	}
 
-	marshal, err := json.Marshal(m)
+	return binBuf.Bytes(), nil
+}
+
+func (b Int32BinarySerializer) decode(input []byte) (int32, int32, error) {
+	buffer0 := bytes.NewBuffer(input[0:4])
+	buffer1 := bytes.NewBuffer(input[4:8])
+	var duration, distance int32
+	err := binary.Read(buffer0, binary.LittleEndian, &duration)
 	if err != nil {
-		return "", err
+		return 0, 0, err
 	}
 
-	return string(marshal), nil
+	err = binary.Read(buffer1, binary.LittleEndian, &distance)
+	if err != nil {
+		return 0, 0, err
+	}
+	return duration, distance, nil
+}
+
+func (b Int32BinarySerializer) encode(duration, distance int32) ([]byte, error) {
+	var binBuf bytes.Buffer
+	err := binary.Write(&binBuf, binary.LittleEndian, duration)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Write(&binBuf, binary.LittleEndian, distance)
+	if err != nil {
+		return nil, err
+	}
+	return binBuf.Bytes(), nil
 }
