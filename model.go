a3c701ad328dc275a8bdb119fd307f6d1f61e842
Rui Xu <rui@nextbillion.ai>
2025-06-18 20:53:29 +0800

    feat: add negative value return

--- model.go
+++ model.go
@@ -15,7 +15,7 @@ type MatrixElement struct {
 }
 
 type Value struct {
-	Value uint32 `json:"value"`
+	Value int32 `json:"value"`
 }
 
 /*
@@ -47,6 +47,7 @@ const (
 type TaskMeta struct {
 	Index      map[string]IndexItem `json:"index"`
 	MatrixInfo MatrixInfo           `json:"matrix_info,omitempty"`
+	version    int                  // 记录版本
 }
 
 type IndexItem struct {
